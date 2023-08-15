c82800c1968bb5c0d59775d4bc748babc772bf7b
Christian Valentim <christianvalentim99@gmail.com>
2023-08-15 17:59:29 -0300

    Feat:(criação do componente Gallery)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './index.css'
 
 // import { Header } from "./components/Header";
-import { SectionOne } from './components/Main 1/SectionOne';
+// import { SectionOne } from './components/Main 1/SectionOne';
+import { Gallery } from './components/Main 1/Gallery';
+
 
 function App() {
   return (
     <>
-      <SectionOne />
+      <Gallery />
     </>
   );
 }
