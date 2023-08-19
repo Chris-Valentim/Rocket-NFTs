c60993fb8c422b82abaefdb5b82cf5ce945145e0
Christian Valentim <christianvalentim99@gmail.com>
2023-08-19 18:56:49 -0300

    Feat:(criação do componente MainFour)

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import './index.css'
 
 // import { MainOne } from './components/Main-1/index'
 // import { MainTwo } from './components/Main-2'
-import { MainThree } from './components/Main-3';
+// import { MainThree } from './components/Main-3';
+// import { MainFour } from './components/Main-4';
+
 
 function App() {
   return (
     <>
       {/* <MainOne /> */}
       {/* <MainTwo /> */}
-      <MainThree />
+      {/* <MainThree /> */}
+      {/* <MainFour /> */}
     </>
   );
 }
---
+++ src/components/Main-4/index.tsx
@@ -0,0 +1,93 @@
+import * as S from './styles';
+
+import arrowOrange from '../../assets/arrow-orange.svg'
+import artistaOne from '../../assets/artista-1.png'
+import artistaTwo from '../../assets/artista-2.png'
+import artistaThree from '../../assets/artista-3.png'
+import artistaFour from '../../assets/artista-4.png'
+import artistaFive from '../../assets/artista-5.png'
+import artistaSix from '../../assets/artista-6.png'
+
+const MainFour = () => {
+  return(
+    <S.Wrapper>
+      <S.Title>Melhores <S.StyledText>
+        artistas</S.StyledText>
+      </S.Title>
+      <S.Content>
+        <S.Card>
+          <img src={artistaOne} alt="artista-One" />
+          <S.InfoCard>
+            <S.AvatarName>
+              Mayk Brito
+            </S.AvatarName>
+            <S.PhotosNumbers>
+              80 Fotografias
+            </S.PhotosNumbers>
+          </S.InfoCard>
+        </S.Card>
+        <S.Featured>
+          <img src={artistaTwo} alt="artista-Two" />
+          <S.FeaturedCard>
+            <S.FeaturedAvatarName>
+              Jakeliny G.
+            </S.FeaturedAvatarName>
+            <S.FeaturedAvatarName>
+              99 Fotografias
+            </S.FeaturedAvatarName>
+          </S.FeaturedCard>
+        </S.Featured>
+        <S.Card>
+          <img src={artistaThree} alt="artista-Three" />
+          <S.InfoCard>
+            <S.AvatarName>
+              João I.
+            </S.AvatarName>
+            <S.PhotosNumbers>
+              50 Fotografias
+            </S.PhotosNumbers>
+          </S.InfoCard>
+        </S.Card>
+        <S.Card>
+          <img src={artistaFour} alt="artista-Four" />
+          <S.InfoCard>
+            <S.AvatarName>
+              Tamires Santos
+            </S.AvatarName>
+            <S.PhotosNumbers>
+              43 Fotografias
+            </S.PhotosNumbers>
+          </S.InfoCard>
+        </S.Card>
+        <S.Card>
+          <img src={artistaFive} alt="artista-Five" />
+          <S.InfoCard>
+            <S.AvatarName>
+              Ana Silva
+            </S.AvatarName>
+            <S.PhotosNumbers>
+              28 Fotografias
+            </S.PhotosNumbers>
+          </S.InfoCard>
+        </S.Card>
+        <S.Card>
+          <img src={artistaSix} alt="artista-Six" />
+          <S.InfoCard>
+            <S.AvatarName>
+              Diego F.
+            </S.AvatarName>
+            <S.PhotosNumbers>
+              20 Fotografias
+            </S.PhotosNumbers>
+          </S.InfoCard>
+        </S.Card>
+      </S.Content>
+      <S.Button href="/">
+        Ver todos artistas 
+        <img src={arrowOrange} alt="arrow-Orange" />
+      </S.Button>
+    </S.Wrapper>
+  )
+}
+
+export { MainFour } 
