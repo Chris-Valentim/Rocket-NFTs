e88ca05b0b2eb86934109e9f1612677c44624a0d
Christian Valentim <christianvalentim99@gmail.com>
2023-08-23 16:18:57 -0300

    Feat:(refatoração do código)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import './index.css'
 
-// import { MainOne } from './components/Main-1/index'
-// import { MainTwo } from './components/Main-2'
-// import { MainThree } from './components/Main-3';
-// import { MainFour } from './components/Main-4';
+import { MainOne } from './components/Main-1/index'
+import { MainTwo } from './components/Main-2'
+import { MainThree } from './components/Main-3';
+import { MainFour } from './components/Main-4';
 import { Footer } from './components/Footer';
 
 
 function App() {
   return (
     <>
-      {/* <MainOne /> */}
-      {/* <MainTwo /> */}
-      {/* <MainThree /> */}
-      {/* <MainFour /> */}
+      <MainOne />
+      <MainTwo />
+      <MainThree />
+      <MainFour />
       <Footer />
     </>
   );
