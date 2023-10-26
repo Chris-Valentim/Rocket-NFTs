f15c3ef8dad5090c67a4b4c99d462672bdff20e6
Christian Valentim <christianvalentim99@gmail.com>
2023-10-25 21:21:51 -0300

    Refatoração do código no arquivo index.tsx; Implementação do media querry no componente MainThree

--- src/components/Main-3/index.tsx
+++ src/components/Main-3/index.tsx
@@ -1,41 +1,70 @@
 import * as S from './styles';
 
-import astronautaOne from '../../assets/astronauta-1.png'
-import astronautaTwo from '../../assets/astronauta-2.png'
-import astronautaThree from '../../assets/astronauta-3.png'
-import astronautaFour from '../../assets/astronauta-4.png'
-import arrowWhite from '../../assets/arrow-white.svg'
-
 const MainThree = () => {
   return (
     <S.Wrapper>
       <S.Title>
-        <S.StyledText>Populares</S.StyledText> da semana
+        <S.StyledText>
+          Populares
+        </S.StyledText> 
+        da semana
       </S.Title>
       <S.Content>
         <div>
           <div>
-            <S.SubTitle>Astronauta 1<img src={arrowWhite} alt="arrow-White" /></S.SubTitle>
-            <S.AvatarName>Mayk Brito <S.StyledText>1.50 RKT</S.StyledText></S.AvatarName>
-            <img src={astronautaOne} alt="astronauta One" />
+            <S.SubTitle>
+              Astronauta 1
+              <S.ImageArrow />
+            </S.SubTitle>
+            <S.AvatarName>
+              Mayk Brito 
+              <S.StyledText>
+                1.50 RKT
+              </S.StyledText>
+            </S.AvatarName>
+            <S.ImageAstronautaOne />
           </div>
           <div>
-            <S.SubTitle>Astronauta 2<img src={arrowWhite} alt="arrow-White" /></S.SubTitle>
-            <S.AvatarName>João Inácio (Biro) <S.StyledText>3.75 RKT</S.StyledText></S.AvatarName>
-            <img src={astronautaTwo} alt="astronauta Two" />
+            <S.SubTitle>
+              Astronauta 2
+              <S.ImageArrow />
+            </S.SubTitle>
+            <S.AvatarName>
+              João Inácio (Biro) 
+              <S.StyledText>
+                3.75 RKT
+              </S.StyledText>
+            </S.AvatarName>
+            <S.ImageAstronautaTwo />
           </div>
         </div>
-        <S.DividingLine></S.DividingLine>
+        <S.DividingLine />
         <div>
           <div>
-            <S.SubTitle>Astronauta 3<img src={arrowWhite} alt="arrow-White" /></S.SubTitle>
-            <S.AvatarName>Jakeliny Gracielly <S.StyledText>2.0 RKT</S.StyledText></S.AvatarName>
-            <img src={astronautaThree} alt="astronauta Three" />
+            <S.SubTitle>
+              Astronauta 3
+              <S.ImageArrow />
+            </S.SubTitle>
+            <S.AvatarName>
+              Jakeliny Gracielly 
+              <S.StyledText>
+                2.0 RKT
+              </S.StyledText>
+            </S.AvatarName>
+            <S.ImageAstronautaThree />
           </div>
           <div>
-            <S.SubTitle>Astronauta 4<img src={arrowWhite} alt="arrow-White" /></S.SubTitle>
-            <S.AvatarName>Tamires Santos <S.StyledText>4.3 RKT</S.StyledText></S.AvatarName>
-            <img src={astronautaFour} alt="astronauta Four" />
+            <S.SubTitle>
+              Astronauta 4
+              <S.ImageArrow />
+            </S.SubTitle>
+            <S.AvatarName>
+              Tamires Santos
+                <S.StyledText> 
+                  4.3 RKT
+                </S.StyledText>
+            </S.AvatarName>
+            <S.ImageAstronautaFour />
           </div>
         </div>
       </S.Content>
--- src/components/Main-3/styles.ts
+++ src/components/Main-3/styles.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components'
 
+import arrowWhite from '../../assets/arrow-white.svg'
+import astronautaOne from '../../assets/astronauta-1.png'
+import astronautaTwo from '../../assets/astronauta-2.png'
+import astronautaThree from '../../assets/astronauta-3.png'
+import astronautaFour from '../../assets/astronauta-4.png'
+
 export const Wrapper = styled.div`
   background-color: var(--body-background);
 
@@ -15,9 +21,15 @@ export const Title = styled.h1`
   font-weight: bold;
   font-size: 50px;
 
-  margin: 0;
   margin-top: 138px;
   margin-bottom: 158px;
+
+  @media (max-width: 425px) {
+    margin-top: 30px;
+    margin-bottom: 30px;
+
+    font-size: 30px;
+  }
 `
 
 export const StyledText = styled.span`
@@ -27,6 +39,13 @@ export const StyledText = styled.span`
 export const Content = styled.div`
   display: flex;
   justify-content: space-around;
+
+  @media (max-width: 425px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `
 
 export const SubTitle = styled.h2`
@@ -51,4 +70,44 @@ export const DividingLine = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`
+
+export const ImageArrow = styled.span`
+  background-image: url(${arrowWhite});
+  background-repeat: no-repeat;
+
+  height: 25px;
+  width: 25px;
+`
+
+export const ImageAstronautaOne = styled.div`
+  background-image: url(${astronautaOne});
+  background-repeat: no-repeat;
+
+  height: 200px;
+  width: 350px;
+`
+
+export const ImageAstronautaTwo = styled.div`
+  background-image: url(${astronautaTwo});
+  background-repeat: no-repeat;
+
+  height: 200px;
+  width: 350px;
+`
+
+export const ImageAstronautaThree = styled.div`
+  background-image: url(${astronautaThree});
+  background-repeat: no-repeat;
+
+  height: 200px;
+  width: 350px;
+`
+
+export const ImageAstronautaFour = styled.div`
+  background-image: url(${astronautaFour});
+  background-repeat: no-repeat;
+
+  height: 200px;
+  width: 350px;
 `
