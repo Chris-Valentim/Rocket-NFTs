db270b5220455ec8eb7a9101ba1392b8e8d06ee5
Christian Valentim <christianvalentim99@gmail.com>
2023-11-24 20:53:02 -0300

    Deploy

--- src/components/Main-1/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-import backgroundImage from '../../assets/background.png'
-
-export const Wrapper = styled.div`
-  background-image: url(${backgroundImage});
-  background-position: center;
-  background-size: cover;
-`
